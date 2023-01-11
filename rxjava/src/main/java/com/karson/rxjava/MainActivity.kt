02efc033faa9f6bc8ce8fb9a28971ff2276e28a4
huangjiachen@phoenixtv.com <hjc123456>
2023-01-11 14:09:01 +0800

    rxjava项目

---
+++ rxjava/src/main/java/com/karson/rxjava/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.karson.rxjava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karson.rxjava.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn.setOnClickListener {
+
+        }
+    }
+}
